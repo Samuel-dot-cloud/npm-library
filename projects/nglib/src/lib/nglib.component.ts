83389258c286d6274467b8d3d059e0f687a0022d
Samuel-dot-cloud <successam336@gmail.com>
2020-09-10 11:46:44 +0300

    Initial library structure

---
+++ projects/nglib/src/lib/nglib.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-nglib',
+  template: `
+    <p>
+      Hello world!!!🔥 The first npm library built with Angular⚡🌋🌟
+    </p>
+    <p>Built with 💌 by Samuel</p>
+  `,
+  styles: [
+  ]
+})
+export class NglibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
